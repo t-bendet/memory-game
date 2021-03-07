9571251f583b4250c20d71014a9bd8664a8b1948
t-bendet <talbendet21@gmail.com>
2021-03-07 15:09:25 +0200

    added randomnes with colors and img

--- memory.js
+++ memory.js
@@ -11,98 +11,80 @@ const gameData = {
   counterData: 0,
   win: false,
 };
-const cardImg = [];
-const cardColor = [
-  "#FF6633",
-  "#FFB399",
-  "#FF33FF",
-  "#FFFF99",
-  "#00B3E6",
-  "#E6B333",
-  "#3366E6",
-  "#999966",
-  "#99FF99",
-  "#B34D4D",
-  "#80B300",
-  "#809900",
-  "#E6B3B3",
-  "#6680B3",
-  "#66991A",
-  "#FF99E6",
-  "#CCFF1A",
-  "#FF1A66",
-  "#E6331A",
-  "#33FFCC",
-  "#66994D",
-  "#B366CC",
-  "#4D8000",
-  "#B33300",
-  "#CC80CC",
-  "#66664D",
-  "#991AFF",
-  "#E666FF",
-  "#4DB3FF",
-  "#1AB399",
-  "#E666B3",
-  "#33991A",
-  "#CC9999",
-  "#B3B31A",
-  "#00E680",
-  "#4D8066",
-  "#809980",
-  "#E6FF80",
-  "#1AFF33",
-  "#999933",
-  "#FF3380",
-  "#CCCC00",
-  "#66E64D",
-  "#4D80CC",
-  "#9900B3",
-  "#E64D66",
-  "#4DB380",
-  "#FF4D4D",
-  "#99E6E6",
-  "#6666FF",
-];
-
+const gameThemes = {
+  cardColor: [
+    "#FF6633",
+    "#FFB399",
+    "#FF33FF",
+    "#FFFF99",
+    "#00B3E6",
+    "#E6B333",
+    "#3366E6",
+    "#999966",
+    "#99FF99",
+    "#B34D4D",
+    "#80B300",
+    "#809900",
+    "#E6B3B3",
+    "#6680B3",
+    "#66991A",
+    "#FF99E6",
+    "#CCFF1A",
+    "#FF1A66",
+    "#E6331A",
+    "#33FFCC",
+    "#66994D",
+  ],
+};
 // each btn contains the data about the game
 startMenu.addEventListener("click", gameOptions);
 function gameOptions(e) {
   if (e.target.dataset.load) {
     let size = e.target.dataset.size;
-    initGame(size);
+    let theme = e.target.dataset.theme;
+    let type = e.target.dataset.type;
+    initGame(size, theme, type);
   }
 }
 //recives the data from random function and deals the cards
-function initGame(num) {
+function initGame(num, deckTheme, Decktype) {
+  let deck = randomizeCardData(num, deckTheme, Decktype);
   body.firstElementChild.remove();
-  // timerSelector init
-  // countrSelector init
-  // for (let i = 0; index < num; i++) {
-  //   card = document.createElement("div")
-  //   boardSelector.
-  // }
+  for (const card of deck) {
+    let div = document.createElement("div");
+    div.setAttribute("data-id", card.id);
+    div.setAttribute("data-theme", card.fill);
+    div.setAttribute("data-type", card.cardType);
+    if (card.cardType == "color") {
+      div.style.backgroundColor = card.fill;
+    } else {
+      div.style["background-image"] = card.fill;
+    }
+    div.classList.add("card");
+    boardSelector.appendChild(div);
+  }
+
+  //timerSelector init
+  //countrSelector init
 }
 
-function randomizeCardData(setNumber, setType) {
-  const cardImg = [...setType];
+//randomized array of card objects with matching id's
+function randomizeCardData(setNumber, setTheme, setType) {
+  let cardType = setType == "color" ? "color" : "img";
+  let cardImg = [...gameThemes[setTheme]];
   let cardsArr = [];
   for (let i = 0; i < setNumber; i++) {
     let imgArrIndex = Math.floor(Math.random() * cardImg.length);
-    //TODO change color to something else
-    let cardObj = { color: cardImg[imgArrIndex], id: i };
+    let cardObj = { fill: cardImg[imgArrIndex], id: i, cardType: cardType };
+    cardsArr.push(cardObj);
+    cardsArr.push(cardObj);
     cardImg.splice(imgArrIndex, 1);
-    cardsArr.push(cardObj);
-    cardsArr.push(cardObj);
-    console.log(imgArrIndex);
   }
   //randomizing array
+  console.log(cardsArr);
   for (let i = cardsArr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [cardsArr[i], cardsArr[j]] = [cardsArr[j], cardsArr[i]];
   }
-  console.log(cardsArr);
   return cardsArr;
 }
-
-randomizeCardData(9, cardColor);
